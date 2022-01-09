e79d546a4081973f95a787b6a48c4d189436ae25
Alex Caissy <alex.dev.prog@gmail.com>
2022-01-09 07:56:32 -0500

    [FIX] - Added the _redirects so Link works when published, and added goBack button

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import "./styles/App.scss";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useMatch, useNavigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PeopleInSpace from "./pages/PeopleInSpace";
 import Tracker from "./pages/Tracker";
 function App() {
-    return (
-        <div className="container">
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/people" element={<PeopleInSpace />} />
-                <Route path="/tracker" element={<Tracker />} />
-            </Routes>
-        </div>
-    );
+  const isHome = useMatch("/");
+  return (
+    <div className="container">
+      {!isHome && (
+        <Link className="go-back-btn" to="/">
+          {"<"}
+        </Link>
+      )}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/people" element={<PeopleInSpace />} />
+        <Route path="/tracker" element={<Tracker />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/PeopleInSpace.jsx
+++ src/pages/PeopleInSpace.jsx
@@ -3,54 +3,54 @@ import axios from "axios";
 import BeatLoader from "react-spinners/BeatLoader";
 
 const PeopleInSpace = () => {
-    const [people, setPeople] = useState();
-    const [isLoading, setIsLoading] = useState(true);
+  const [people, setPeople] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchPeople = async () => {
-            try {
-                const response = await axios.get(
-                    `${
-                        process.env.NODE_ENV === "production"
-                            ? "/api"
-                            : "http://api.open-notify.org"
-                    }/astros.json`
-                );
-                console.log("response: ", response);
-                setPeople(response.data.people);
-                setIsLoading(false);
-            } catch (error) {
-                console.error(error);
-                setIsLoading(true);
-            }
-        };
-        fetchPeople();
-    }, []);
+  useEffect(() => {
+    const fetchPeople = async () => {
+      try {
+        const response = await axios.get(
+          `${
+            process.env.NODE_ENV === "production"
+              ? "/api"
+              : "http://api.open-notify.org"
+          }/astros.json`
+        );
+        console.log("response: ", response);
+        setPeople(response.data.people);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        setIsLoading(true);
+      }
+    };
+    fetchPeople();
+  }, []);
 
-    return (
-        <div className="container">
-            <div className="astronauts" />
-            <BeatLoader color="#1e2739" loading={isLoading} size={200} />
-            {!isLoading && (
-                <div className="peopleInSpace">
-                    {people &&
-                        people.map((person, idx) => (
-                            <a
-                                key={person.name}
-                                className="names"
-                                href={`https://en.wikipedia.org/wiki/${person.name}`}
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <p>
-                                    {person.name} - {person.craft}
-                                </p>
-                            </a>
-                        ))}
-                </div>
-            )}
+  return (
+    <div className="container">
+      <div className="astronauts" />
+      <BeatLoader color="#1e2739" loading={isLoading} size={200} />
+      {!isLoading && (
+        <div className="peopleInSpace">
+          {people &&
+            people.map((person, idx) => (
+              <a
+                key={person.name}
+                className="names"
+                href={`https://en.wikipedia.org/wiki/${person.name}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p>
+                  {person.name} - {person.craft}
+                </p>
+              </a>
+            ))}
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default PeopleInSpace;
